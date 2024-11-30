a87a927eb1e032d4bd030ad2ff7c497807c63c6f
BekzhanK1 <bkimadieff@gmail.com>
2024-12-01 02:20:04 +0500

    fix

--- chat/urls.py
+++ chat/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ChatHistoryView
+from .views import ChatHistoryView, ListChatRoomsView
 
 urlpatterns = [
+    path("chats/", ListChatRoomsView.as_view(), name="chats"),
     path("history/<str:room_name>/", ChatHistoryView.as_view(), name="chat-history"),
 ]
--- chat/views.py
+++ chat/views.py
@@ -2,9 +2,52 @@ from django.contrib.auth import get_user_model
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
-from .models import Room
+from .models import Room, Message
 
 User = get_user_model()  # Use the custom User model if applicable
+
+
+class ListChatRoomsView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        # Get rooms where the user is either user1 or user2
+        rooms = Room.objects.filter(user1_id=user.id)
+        rooms2 = Room.objects.filter(user2_id=user.id)
+        rooms = list(rooms) + list(rooms2)
+
+        response = []
+        for room in rooms:
+            # Get the last message in the room
+            last_message = (
+                Message.objects.filter(room=room).order_by("-timestamp").first()
+            )
+
+            # Determine the companion's ID
+            companion_id = room.user2_id if room.user1_id == user.id else room.user1_id
+            companion = User.objects.get(id=companion_id)
+
+            # Construct response for each room
+            response.append(
+                {
+                    "room_name": room.name,
+                    "companion": {
+                        "id": companion.id,
+                        "full_name": f"{companion.first_name} {companion.last_name}",
+                        "email": companion.email,
+                    },
+                    "last_message": {
+                        "who": (
+                            "me" if last_message.sender_id == user.id else "companion"
+                        ),
+                        "message": last_message.message,
+                        "timestamp": last_message.timestamp,
+                    },
+                }
+            )
+
+        return Response(response)
 
 
 class ChatHistoryView(APIView):
