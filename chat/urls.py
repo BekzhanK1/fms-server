294a8abb8f03cdea6318318bcd6fd0b330e0431c
BekzhanK1 <bkimadieff@gmail.com>
2024-12-01 02:45:08 +0500

    fix

--- chat/urls.py
+++ chat/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ChatHistoryView, ListChatRoomsView
 
 urlpatterns = [
-    path("chats/", ListChatRoomsView.as_view(), name="chats"),
+    path("rooms/", ListChatRoomsView.as_view(), name="chats"),
     path("history/<str:room_name>/", ChatHistoryView.as_view(), name="chat-history"),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,4 @@
+import json
 from django.contrib.auth import get_user_model
 from rest_framework.response import Response
 from rest_framework.views import APIView
